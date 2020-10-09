88a986bcd9a446b9cbb2f43e366ad7ddbd9058e9
Juliano H. Foleis <julianofoleiss@utfpr.edu.br>
2020-10-09 13:20:34 -0300

    acrescentei gerador de arquivo dot p/ exprtree

--- bin_trees/expr_tree/exprtree.c
+++ bin_trees/expr_tree/exprtree.c
@@ -19,3 +19,47 @@ void ET_Destruir(ExprTree *A){
     ET_Destruir(A->dir);
     free(A);
 }
+
+// static int ET_GenDOT_R(ExprTree *A, FILE* saida, int *id){
+//     int e, d, me;
+
+//     if(A == NULL)
+//         return 0;
+
+//     e = ET_GenDOT_R(A->esq, saida, id);
+//     d = ET_GenDOT_R(A->dir, saida, id);
+
+//     me = (*id)++;
+
+//     if(A->tipo_no == ETTN_CONSTANTE)
+//         fprintf(saida, "%d [label=\"%d\"];\n", me, A->dado.constante);
+//     else
+//         fprintf(saida, "%d [label=\"%c\"];\n", me, A->dado.op);
+
+//     if(e || d){
+//         if(e)
+//             fprintf(saida, "%d -> %d;\n", me, e);
+//         if(d)
+//             fprintf(saida, "%d -> %d;\n", me, d);
+//     }
+
+//     return me;
+
+// }
+
+// void ET_GenDOT(ExprTree *A, const char* arq_saida){
+
+//     FILE* saida;
+//     int id = 1;
+
+//     saida = fopen(arq_saida, "w");
+
+//     fprintf(saida, "digraph G {\n");
+
+//     ET_GenDOT_R(A, saida, &id);
+
+//     fprintf(saida, "}\n");
+
+//     fclose(saida);
+
+// }

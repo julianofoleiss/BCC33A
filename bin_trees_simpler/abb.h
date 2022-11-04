74a8d6d5eb1ee625eba0332c8082f7bd31b23d9c
julianofoleiss <julianofoleiss@utfpr.edu.br>
2022-11-04 13:00:49 -0300

    add uma estrutura de ABB sem end. de ponteiro

---
+++ bin_trees_simpler/abb.h
@@ -0,0 +1,26 @@
+#pragma once
+
+typedef struct NOH{
+    int chave;
+    int valor;
+    struct NOH *esq, *dir;
+}NOH;
+
+typedef struct ABB{
+    NOH* raiz;
+}ABB;
+
+ABB* ABB_Criar();
+
+// Retorna o endereço no nó que contém a chave na árvore A.
+// Caso a chave não esteja na árvore, retorna NULL
+NOH* ABB_Buscar(ABB* A, int chave);
+
+
+// Insere o par (chave, valor) na árvore A. 
+// Caso a chave já esteja na árvore, o valor é atualizado.
+void ABB_Inserir(ABB*A , int chave, int valor);
+
+// remove a chave da árvore A.
+// Caso a chave não esteja na árvore, ela não é alterada.
+void ABB_Remover(ABB* A, int chave);

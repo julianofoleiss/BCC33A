926d0aa1bb8dd040a2c1fd7af66535c703a4f09c
Juliano Foleis <julianofoleiss@utfpr.edu.br>
2023-05-22 18:00:37 -0300

    ajustes na ExprTree

--- bin_trees/expr_tree/exprtree.h
+++ bin_trees/expr_tree/exprtree.h
@@ -3,8 +3,6 @@
 typedef struct ExprTree{
     struct ExprTree *esq;
     struct ExprTree *dir;
-    int dado;
 }ExprTree;
 
-ExprTree* ET_Criar(int dado, ExprTree *e, ExprTree *d);
 void ET_Destruir(ExprTree *A);
